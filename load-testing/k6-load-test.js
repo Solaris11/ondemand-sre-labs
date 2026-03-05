64a9b276e61326dd273ba1a30d22460fe4017723
Emrah Bayram <gokce.bayram@gmail.com>
2026-03-04 22:45:30 -0800

    Adding CPU threshold

---
+++ load-testing/k6-load-test.js
@@ -0,0 +1,12 @@
+import http from "k6/http";
+import { sleep } from "k6";
+
+export const options = {
+  vus: 30,
+  duration: "90s",
+};
+
+export default function () {
+  http.get("http://localhost:8080/");
+  sleep(0.1);
+} 
